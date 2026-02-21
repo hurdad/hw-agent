305a5aa466e65bec214f1c0d94c7757a12997386
Alexander Hurd <hurdad@gmail.com>
2026-02-20 22:40:41 -0500

    Handle SIGTERM/SIGINT for graceful agent shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
+#include <csignal>
 #include <iostream>
 #include <string>
 
 #include "core/agent.hpp"
 #include "core/config.hpp"
 
+namespace {
+
+volatile std::sig_atomic_t g_shutdown_requested = 0;
+
+void handle_shutdown_signal(int /*signal*/) {
+  g_shutdown_requested = 1;
+}
+
+}  // namespace
+
 int main(int argc, char** argv) {
+  std::signal(SIGINT, handle_shutdown_signal);
+  std::signal(SIGTERM, handle_shutdown_signal);
+
   const std::string config_path = argc > 1 ? argv[1] : "configs/agent.yaml";
 
   hw_agent::core::AgentConfig config{};
@@ -16,7 +30,11 @@ int main(int argc, char** argv) {
   }
 
   hw_agent::core::Agent agent{config};
-  agent.run_for_ticks(0);
+  while (g_shutdown_requested == 0) {
+    agent.run_for_ticks(1);
+  }
+
+  std::cerr << "[agent] shutdown signal received; exiting cleanly\n";
 
   return 0;
 }
