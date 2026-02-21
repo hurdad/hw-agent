707833cc14ad624e0580ca1c5bda2b3bb8f60ee5
Alexander Hurd <hurdad@gmail.com>
2026-02-20 22:27:40 -0500

    Fix Redis TS publish timestamps to use Unix time

--- include/core/timestamp.hpp
+++ include/core/timestamp.hpp
@@ -11,4 +11,10 @@ inline std::uint64_t timestamp_now_ns() {
       std::chrono::duration_cast<std::chrono::nanoseconds>(now).count());
 }
 
+inline std::uint64_t unix_timestamp_now_ns() {
+  const auto now = std::chrono::system_clock::now().time_since_epoch();
+  return static_cast<std::uint64_t>(
+      std::chrono::duration_cast<std::chrono::nanoseconds>(now).count());
+}
+
 }  // namespace hw_agent::core
